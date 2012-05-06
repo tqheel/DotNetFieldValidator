8d71373608fd51c429fa2e19c077ede55a853acd
Todd Qualls <tqualls@gmail.com>
2012-05-06 14:44:38 -0400

    Added validation and utility classes.

---
+++ App_Code/Utils.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text.RegularExpressions;
+using AICPA_BAPModel;
+using System.Net.Mail;
+using System.Configuration;
+
+/// <summary>
+/// Collection of public utility methods for various common functions
+/// </summary>
+public class Utils
+{
+    
+
+    public static bool IsValidEmail(string strIn)
+    {
+        // Return true if strIn is in valid e-mail format.
+        return Regex.IsMatch(strIn,
+               @"^(?("")("".+?""@)|(([0-9a-zA-Z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-zA-Z])@))" +
+               @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,6}))$");
+    }
+
+    public static bool IsFieldPopulated(string fieldValue)
+    {
+        int charCount = fieldValue.Length;
+        if (charCount < 1) return false;
+        else return true;
+    }
+
+    public static string FormatDecimalToFixed2(decimal? valueToFormat)
+    {
+        string specifier = "{0:0.00}";
+        if (valueToFormat == null) return "0.00";
+        else return string.Format(specifier,valueToFormat);
+    }
+
+    public static string FormatNumberToCommaNoDecimals(int? valueToFormat)
+    {
+        string specifier = "{0:0,000}";
+        if (valueToFormat == null) return "0";
+        else return string.Format(specifier, valueToFormat);
+    }
+
+    public static string FormatDecimalToComma2Decimals(decimal? valueToFormat)
+    {
+        string specifier = "{0:0,000.00}";
+        if (valueToFormat == null) return "0";
+        else return string.Format(specifier, valueToFormat);
+    }
+
+    public static DateTime ConvertTimeToETZ(DateTime timeToConvert)
+    {
+        const string etz = "Eastern Standard Time";
+        TimeZoneInfo tzTo = TimeZoneInfo.FindSystemTimeZoneById(etz);
+        DateTime convertedTime = TimeZoneInfo.ConvertTimeFromUtc(timeToConvert.ToUniversalTime(), tzTo);
+        return convertedTime;
+    }
+
+    public static string ComposeErrMsg(string input)
+    {
+        string output = string.Empty;
+        string prefix = "<li>";
+        string suffix = "</li>";
+        return output = prefix + input + suffix;
+    }
+
+    
+
+    public static bool IsStringShortEnough(string input, int maxLength)
+    {
+        if (input == null) return true;
+        if ( input.Length <= maxLength) return true;
+        else return false;
+    }
+    
+    public static bool IsValidUSZip(string num)
+    {
+        // Allows 5 digit, 5+4 digit and 9 digit zip codes
+        string pattern = @"^(\d{5}-\d{4}|\d{5}|\d{9})$";
+        Regex match = new Regex(pattern);
+        return match.IsMatch(num);
+    }
+
+
+    public static DateTime GetLocalBapTimeStamp()
+    {
+        return ConvertTimeToETZ(DateTime.Now);
+    }
+
+
+    public static bool IsValidUSOrCanadaPhone(string phone)
+    {
+        // Match a US or Canadian phone number with an optional 0 or 1, optional separator of  -, . or space, optional parentheses
+        string pattern = @"^[01]?[- .]?\(?[2-9]\d{2}\)?[- .]?\d{3}[- .]?\d{4}$";
+        Regex match = new Regex(pattern);
+        return match.IsMatch(phone);
+    }
+
+    public static bool IsValidIntlPhone (string phone)
+    {
+        string pattern = @"^(\+[1-9][0-9]*(\([0-9]*\)|-[0-9]*-))?[0]?[1-9][0-9\- ]*$";
+        Regex match = new Regex(pattern);
+        return match.IsMatch(phone);
+    }
+
+    
+}
+
+
+
--- App_Code/WebService.cs
+++ App_Code/WebService.cs
@@ -2,23 +2,70 @@
 using System.Collections.Generic;
 using System.Web;
 using System.Web.Services;
+using System.Web.Script.Services;
 
-/// <summary>
-/// Summary description for WebService
-/// </summary>
+
 [WebService(Namespace = "http://tempuri.org/")]
 [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
 public class WebService : System.Web.Services.WebService {
 
-    public WebService () {
+    [WebMethod(EnableSession = true)]
+    [ScriptMethod(ResponseFormat = ResponseFormat.Json, UseHttpGet = false)]
 
-        //Uncomment the following line if using designed components 
-        //InitializeComponent(); 
-    }
+    public void SaveDataFromWebForm(int RecordID, string FirstName, string LastName, string Email, string PhoneNumber)
+    {
+        //Validate Each Parameter/Field from web form and concatenate error message for each field that fails
+        string errorMsg = string.Empty;
+        bool errorsCaught = false;
 
-    [WebMethod]
-    public string HelloWorld() {
-        return "Hello World";
+        //First make sure string fields are short enough to store in our database; for this example we are assuming the string fields are being stored in varchar(50) fields in the db.
+        if(!Utils.IsStringShortEnough(FirstName, 50))
+        {
+            errorMsg+=Utils.ComposeErrMsg("First Name must be 50 characters or less.");
+            errorsCaught=true;
+        }
+
+        if(!Utils.IsStringShortEnough(LastName, 50))
+        {
+            errorMsg+=Utils.ComposeErrMsg("Last Name must be 50 characters or less.");
+            errorsCaught=true;
+        }
+
+        if(!Utils.IsStringShortEnough(Email, 50))
+        {
+            errorMsg+=Utils.ComposeErrMsg("E-mail must be 50 characters or less.");
+            errorsCaught=true;
+        }
+
+        if(!Utils.IsStringShortEnough(PhoneNumber, 50))
+        {
+            errorMsg+=Utils.ComposeErrMsg("Phone number must be 50 characters or less.");
+            errorsCaught=true;
+        }
+
+        //Now validate that the actual email address is a valid address
+        if (!Utils.IsValidEmail(Email))
+        {
+            errorMsg += Utils.ComposeErrMsg("The e-mail address provided is not valid.");
+            errorsCaught = true;
+        }
+        //Now check the phone number as valid US number
+        if (!Utils.IsValidUSOrCanadaPhone(PhoneNumber))
+        {
+            errorMsg += Utils.ComposeErrMsg("The phone number provided is not valid.");
+            errorsCaught = true;
+        }
+
+        //If any validation failed, throw exception, with concatenated error message, which is formatted as a set of HTML <li> elements
+        if (errorsCaught) throw new Exception(errorMsg);
+
+        //Note: there are a few other validation methods in the Utils class not used in above code. Check 'em out...
+            //For example, there is code for ensuring a server datestamp gets converted to Eastern time zone
+
+        //If we get to this point in the method without an exception being raised, then validation succeeded and we persist form data to DB
+        {
+            //Do save to db stuff here...
+        }
     }
     
 }
